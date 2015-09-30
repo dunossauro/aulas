42ca8a166fd4748e6f9df88406bb2d1428258c19
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-09-30 18:07:11 -0300

    Create Cadastro.aspx.cs

---
+++ IES400A/WebSite1/Cadastro.aspx.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Cadastro : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Gravar_Click(object sender, EventArgs e)
+    {
+        //string de conexao com o banco de dados "Access provedor  Microsoft.ACE.OLEDB.12.0"
+        string conexao = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+            Server.MapPath("~/Dados/Cadastro.accdb") + ";Persist Security Info=false";
+
+        //Query SQL para inserir um registro
+        string comando = "INSERT INTO Clientes(Nome,Email,Telefone) VALUES('" +
+            NomeCliente.Text + "','" + EmailCliente.Text + "','" + TelefoneCliente.Text + "');";
+
+        AppDatabase.OleDBTransaction ole = new AppDatabase.OleDBTransaction();
+        ole.ConnectionString = conexao;
+        ole.Query(comando);
+    }
+}
