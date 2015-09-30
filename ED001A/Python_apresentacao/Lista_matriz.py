687cf4fe115236ca80fab7a87f8e66027fb656ab
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2015-09-30 18:15:31 -0300

    Create Lista_matriz.py

---
+++ ED001A/Python_apresentacao/Lista_matriz.py
@@ -0,0 +1,84 @@
+from os import system
+
+lista = []
+
+def inserir():
+
+	cod = input("Insira o código: ")
+	banda = input("insira a banda: ")
+	cd = input("insira o cd: ")
+	ano = input("insira o ano: ")
+
+	lista.append([cod,banda,cd,ano])
+
+	input("\n\nDigite enter para continuar")
+
+def buscar():
+	if lista == []:
+		print("Lista vazia")
+
+	else:
+		cod = input("Insira o valor: ")
+		pos = 0
+		for linha in lista:
+			for coluna in linha:
+				if coluna == cod:
+					print(linha)
+				pos += 1
+		
+	input("\n\nDigite enter para continuar")
+
+#FAZER
+def remover():
+
+	if lista == []:
+		print("Lista vazia")
+
+	else:	
+		cod = int(input("Insira o código: "))
+		pos = 0
+		for linha in lista:
+			if linha[pos][0] == cod:
+				print(linha, "deletada")
+				print(linha[pos][0])
+				#del(lista[cod])
+			pos += 1
+	input("\n\nDigite enter para continuar")
+
+def imprimir():
+
+	if lista == []:
+		print("Lista vazia")
+	else:
+		for x in lista:
+			print(x)
+
+	input("\n\nDigite enter para continuar")
+
+def menu():
+
+	system("clear")
+	print("1. Inserir")
+	print("2. Imprimir")
+	print("3. Buscar")
+	print("4. Remover")
+	print("5. Sair")
+
+	opt = input("Digite a opção desejada: ")
+
+	if opt is "1":
+		inserir()
+	elif opt is "2":
+		imprimir()
+	elif opt is "3":
+		buscar()
+	elif opt is "4":
+		remover()
+	else:
+		exit()
+
+def main():
+	while True:
+		menu()
+
+main()
